6b516dc76998a32ce439c6d0ce3a713abad8a847
renz.ayala <renzayala98@gmail.com>
2026-03-21 21:48:21 -0500

    fix cors

--- src/main/java/com/sunarp/consultasreplaqueamiento/controller/PlacaController.java
+++ src/main/java/com/sunarp/consultasreplaqueamiento/controller/PlacaController.java
@@ -9,6 +9,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/placas")
 public class PlacaController {
